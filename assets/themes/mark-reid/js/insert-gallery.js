c40f9f748ba9e2d20e4b907b35339e263fe340e4
Paul McCarthy <pauld.mccarthy@gmail.com>
2013-03-02 18:39:03 +1300

    images are sorted by name, changed fonts, next/prev links are below image gallery

--- assets/themes/mark-reid/js/insert-gallery.js
+++ assets/themes/mark-reid/js/insert-gallery.js
@@ -6,8 +6,17 @@ $(function() {
     var user_id  = $el.attr("user_id");
     var album_id = $el.attr("album_id");
 
+    var image_cmp = function(img1, img2) {
+      var i1n = img1.title.slice(0, img1.title.lastIndexOf('.'));
+      var i2n = img2.title.slice(0, img2.title.lastIndexOf('.'));
+      return parseInt(i1n) - parseInt(i2n);
+    };
+
     $el.picasaGallery(user_id, album_id, function(images) {
       $ul = $('<ul class="picasa-album"></ul>');
+
+      images.sort(image_cmp);
+
       $.each(images, function(i, img) {
         var $li  = $('<li class="picasa-image"></li>'),
             $a   = $('<a class="picasa-image-large"></a>').attr('href', img.url),
